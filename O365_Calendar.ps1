f3992c36f06813ddb1229f89a74dd53bde516a01
blinkii <blinkii@live.fr>
2023-11-27 15:57:02 +0100

    Update O365_Calendar.ps1
    v0.2
    --> Add modify menu

--- O365_Calendar.ps1
+++ O365_Calendar.ps1
@@ -2,12 +2,15 @@ $continue = $true
 while ($continue){
   write-host “"
   write-host “-------------------- O365 Modif Calendrier ----------------------”
+  write-host “------------------------- Version 0.2 ---------------------------”
+  write-host ""
   write-host “1/ Connexion MS O365”
   write-host “2/ Voir droit utilisateur”
   write-host "3/ Ajouter droit utiliseur"
   write-host "4/ Supprimer droit utiliseur"
+  write-host "5/ Modifier droit utiliseur"
   write-host "X/ Quitter"
-  write-host "--------------------------------------------------------"
+  write-host “-----------------------------------------------------------------”
   write-host “"
   $choix = read-host “Faites un choix ”
   switch ($choix){
@@ -21,12 +24,19 @@ while ($continue){
     3{
         $mail = Read-Host "Entrez le mail complet du collaborateur"
         $mail2 = Read-Host "Entrez le mail complet de la personne a ajouter en admin"
-        Add-MailboxFolderPermission -Identity ${mail}:\Calendrier -User ${mail2} -AccessRights editor
+        $rights = Read-Host "Entrez le nom du droits (None, AvailabilityOnly, LimitedDetails, Editor)"
+        Add-MailboxFolderPermission -Identity ${mail}:\Calendrier -User ${mail2} -AccessRights ${rights}
         }
     4{
         $mail = Read-Host "Entrez le mail complet du collaborateur à modifier"
         $mail2 = Read-Host "Entrez le mail complet de la personne à retrier"
         Remove-MailboxFolderPermission -Identity ${mail}:\Calendrier -User ${mail2}
+        }
+    5{
+        $mail = Read-Host "Entrez le mail complet du collaborateur"
+        $mail2 = Read-Host "Entrez le mail complet de la personne a ajouter en admin"
+        $rights = Read-Host "Entrez le nom du droits (None, AvailabilityOnly, LimitedDetails, Editor)"
+        Set-MailboxFolderPermission -Identity ${mail}:\Calendrier -User ${mail2} -AccessRights ${rights}
         }
     ‘x’ {$continue = $false}
     default {Write-Host "Choix invalide"-ForegroundColor Red}
